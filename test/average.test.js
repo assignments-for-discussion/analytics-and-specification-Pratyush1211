cb0517180e9d073b83a84162fdbf3ed6cc78560b
Pratyush Motha <67197988+Pratyush1211@users.noreply.github.com>
2021-12-19 20:17:15 +0530

    average test

--- test/average.test.js
+++ test/average.test.js
@@ -7,10 +7,10 @@ it('computes average of a list of numbers', ()=> {
   expect(average([1, 2, 3, 4])).to.equal(2.5);
 });
 
-// it('reports the average as NaN on an empty list', ()=> {
-//   expect(average([])).to.be.NaN;
-// });
-
-it('ignores NaN in the input', ()=> {
-  expect(average([1, NaN, 2])).to.equal(1.5);
+it('reports the average as NaN on an empty list', ()=> {
+  expect(average([])).to.be.NaN;
 });
+
+// it('ignores NaN in the input', ()=> {
+//   expect(average([1, NaN, 2])).to.equal(1.5);
+// });
